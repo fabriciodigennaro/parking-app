ea6e9c7509419e4b8521ecba12c71d347276ae21
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2023-08-06 15:31:21 +0200

    add header component and base styles

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  getMenuToggleClass() {
+    return this.isMenuOpen ? 'close' : '';
+  }
+
+}
