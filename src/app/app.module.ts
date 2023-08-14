84db32793f2c73d9c1d7b3c8b33cc1312bdeda41
Fabricio Adrian Di gennaro <58400837+fabriciodigennaro@users.noreply.github.com>
2023-08-14 16:08:21 +0200

    Feature/10 create home page (#1)
    * add home component and redirections
    
    * add button component
    
    * delete default text from home component
    
    * change button color
    
    * fix scroll in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
