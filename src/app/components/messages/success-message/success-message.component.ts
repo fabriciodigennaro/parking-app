d0eaec3b954df902cfa0c41b6a1dd85b808ffc6b
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2023-09-30 14:52:32 +0200

    add UI to success message

--- src/app/components/messages/success-message/success-message.component.ts
+++ src/app/components/messages/success-message/success-message.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-success-message',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./success-message.component.scss']
 })
 export class SuccessMessageComponent {
+  finishingTime: string = 'your parking ends at: 12:25HS';
 
 }
